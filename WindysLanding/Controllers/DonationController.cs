d09cbec37de81609c9d9a7980325ce6cedb83bc0
pizzahunter239 <pizzahunter239@gmail.com>
2026-03-10 17:29:10 -0400

    Donation page and small changes

---
+++ WindysLanding/Controllers/DonationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WindysLanding.Controllers
+{
+    public class DonationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
